c30df35d008102036f22d288495f5217b32161e6
Liu Shaohui <liushaohui@xiaomi.com>
2015-01-05 20:31:16 +0800

    Gen maven project by make pom.xml

--- build-aux/create-src-dir-overlay.sh
+++ build-aux/create-src-dir-overlay.sh
@@ -2,16 +2,20 @@
 # that the overlay matches Java package hierarchy.
 #!/usr/bin/env bash
 
+mkdir maven
+cd maven
 if [ ! -d src-main ]; then
   mkdir src-main
+  (ln -s ../../build-aux/gen_build_data.sh src-main/)
   mkdir src-main/net
   mkdir src-main/tsd
-  (cd src-main/net && ln -s ../../src opentsdb)
-  (cd src-main/tsd && ln -s ../../src/tsd/QueryUi.gwt.xml QueryUi.gwt.xml)
-  (cd src-main/tsd && ln -s ../../src/tsd/client client)
+  (cd src-main/net && ln -s ../../../src opentsdb)
+  (cd src-main/tsd && ln -s ../../../src/tsd/QueryUi.gwt.xml QueryUi.gwt.xml)
+  (cd src-main/tsd && ln -s ../../../src/tsd/client client)
 fi
 if [ ! -d src-test ]; then
   mkdir src-test
   mkdir src-test/net
-  (cd src-test/net && ln -s ../../test opentsdb)
+  (cd src-test/net && ln -s ../../../test opentsdb)
 fi
+
